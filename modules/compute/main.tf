76621e402ec4482d49c02f2b65bccc5086c9f187
priya785467 <90323046+priya785467@users.noreply.github.com>
2022-11-29 16:17:59 +0530

    Update main.tf

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,17 +1,29 @@
-resource "azurerm_app_service_plan" "dev" {
-  name                = "dev"
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = "myappservice-plan"
+  location            = var.location
   resource_group_name = var.resource_group
-  location = var.location
+
   sku {
     tier = "Free"
     size = "F1"
   }
 }
 
-resource "azurerm_app_service" "dev7872" {
-  
+resource "azurerm_app_service" "app_service" {
+  name                = "mywebapp-976888 "
+  location            = var.location
   resource_group_name = var.resource_group
-  location = var.location
-  app_service_plan_id = "${azurerm_app_service_plan.dev.id}"
+  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+
+  #(Optional)
+  site_config {
+dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+  
+  #(Optional)
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
 
 }
